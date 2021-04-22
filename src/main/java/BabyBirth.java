203072b71b72e8d9f37548f08a8e43c291bb8a94
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-04-22 12:57:09 +0300

    Refactor "Printnames()" method

--- src/main/java/BabyBirth.java
+++ src/main/java/BabyBirth.java
@@ -5,21 +5,24 @@ public class BabyBirth {
     public void Printnames() {
         FileResource fr = new FileResource();
         String gender;
+        int numBorn;
 
         for (CSVRecord record : fr.getCSVParser(false)) { // CSV file does not have a header row
+            numBorn = Integer.parseInt(record.get(2));
 
             if (record.get(1).equals("F")) { gender = "Female"; }
             else { gender = "Male"; }
 
-            System.out.println("Name is " + record.get(0)
-                    + ", " + gender
-                    + ", Num born is " + record.get(2));
+            if (numBorn<=100) {
+                System.out.println("Name is " + record.get(0)
+                        + ", " + gender
+                        + ", Num born is " + numBorn);
+            }
         }
     }
 
     public static void main(String[] args) {
         BabyBirth o = new BabyBirth();
-        //o.testHottestInDay();
         o.Printnames();
     }
 }
