ad9b4d99af2963f93d4db4becd4e572875f3d093
kpriluch <Konstantin.Priluchnyi@telekom.com>
2021-04-22 12:37:48 +0300

    Add "Printnames()" method

--- src/main/java/BabyBirth.java
+++ src/main/java/BabyBirth.java
@@ -1,2 +1,25 @@
+import edu.duke.*;
+import org.apache.commons.csv.*;
+
 public class BabyBirth {
+    public void Printnames() {
+        FileResource fr = new FileResource();
+        String gender;
+
+        for (CSVRecord record : fr.getCSVParser(false)) { // CSV file does not have a header row
+
+            if (record.get(1).equals("F")) { gender = "Female"; }
+            else { gender = "Male"; }
+
+            System.out.println("Name is " + record.get(0)
+                    + ", " + gender
+                    + ", Num born is " + record.get(2));
+        }
+    }
+
+    public static void main(String[] args) {
+        BabyBirth o = new BabyBirth();
+        //o.testHottestInDay();
+        o.Printnames();
+    }
 }
